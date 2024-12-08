205e5bc3c165f76d463a93e96d58fa5d1ea8df50
BuildTools <unconfigured@null.spigotmc.org>
2024-12-07 20:41:00 -0500

    Cleaning some formatting -Codie

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -14,6 +14,7 @@ namespace TeamApi
         {
         }
 
+
         // Optional: Override OnConfiguring for design-time support
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
